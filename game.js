db977556c57f1a895eaf801ba8e47c782a87b3cf
Heitor <51799145+heitorln@users.noreply.github.com>
2019-10-04 21:04:48 -0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,20 +1,26 @@
 var Squirrel;
-var Hazelnut;
+var Hazelnut =[];
 var Left;
 var Right;
 var Background;
 var BGMusic;
+//TUGA
+var Score;
+var sco = 0;
+var lives = 3;
 
 //initializing game (this includes canvas, listeners and constant updating of game area) and components
 function startGame() {
     gameArea.start();
     Background = new component(gameArea.canvas.width, gameArea.canvas.height, "resources/bg.jpg", 0, 0, "image");
     Squirrel = new component(64, 64, "resources/squirrel.png", gameArea.canvas.width / 2 - 32, gameArea.canvas.height - 64, "image");
-    Hazelnut = new component(64, 64, "resources/hazelnut.png", 30, 30, "image");
     Left = new component(gameArea.canvas.width / 2, gameArea.canvas.height / 2, "rgba(255, 255, 255, 0.5)", 0, gameArea.canvas.height - (gameArea.canvas.height / 2));
     Right = new component(gameArea.canvas.width / 2, gameArea.canvas.height / 2, "rgba(255, 255, 255, 0.5)", gameArea.canvas.width - gameArea.canvas.width / 2, gameArea.canvas.height - (gameArea.canvas.height / 2));
-
+    //TUGA
+    Score = new component("30px", "Consolas", "black", 200, 40, "text");
 }
+
+
 
 var gameArea = {
     canvas : document.createElement("canvas"),
@@ -25,6 +31,8 @@ var gameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        //tuga
+        this.frame = 0;
 
         //listening for mouse click
 		window.addEventListener('mousedown', function (e) {
@@ -57,6 +65,7 @@ var gameArea = {
     }
 }
 
+
 function component(width, height, color, x, y, type) {
     this.type = type;
     
@@ -64,7 +73,7 @@ function component(width, height, color, x, y, type) {
 	    this.image = new Image();
 	    this.image.src = color;
   	}
-    
+  	
     this.width = width;
     this.height = height;
     this.x = x;
@@ -77,11 +86,19 @@ function component(width, height, color, x, y, type) {
 
 		if (type == "image") {
       	context.drawImage(this.image, this.x, this.y, this.width, this.height);
-
-    	} else {
+        }
+      	//tuga
+  		else if (this.type == "text") {
+      		context.font = this.width + " " + this.height;
+      		context.fillStyle = color;
+      		context.fillText(this.text, this.x, this.y);
+    	}
+    
+    	else {
 	        context.fillStyle = "rgba(255, 255, 255, 0)";
 	        context.fillRect(this.x, this.y, this.width, this.height);
     	}
+
     }   
 
     // checking if "buttons" were clicked
@@ -95,11 +112,34 @@ function component(width, height, color, x, y, type) {
 		return clicked;
   	}
 
+  	// TUGA
+  	
+
+  	this.collect = function(otherobj) {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + (otherobj.width);
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + (otherobj.height);
+    var collected = true;
+    if ((mybottom < othertop) ||
+    (mytop > otherbottom) ||
+    (myright < otherleft) ||
+    (myleft > otherright)) {
+      collected = false;
+    }
+    
+    return collected;
+  }
+
 }   
 
 function updateGameArea() {
-    
     gameArea.clear();
+    gameArea.frame += 1;
 
     if (gameArea.x && gameArea.y) {
 	    if (Left.clicked()) {
@@ -109,10 +149,30 @@ function updateGameArea() {
 	      Squirrel.x += 7;
 	    }
  	}
-
+ 	for(i = 0; i<Hazelnut.length; i += 1){
+		if (Squirrel.collect(Hazelnut[i])){
+ 			sco += 1;
+ 			Hazelnut.splice(i);
+ 		}
+ 		if (Hazelnut[i].y > gameArea.canvas.height+64){
+ 			Hazelnut.splice(i);
+ 			lives -= 1;
+ 		}
+ 	}
+ 	if (gameArea.frame % 50 == 0 || gameArea.frame % 72 == 0 || gameArea.frame % 123 == 0){
+ 		nuty = -50;
+ 		nutx = Math.random()*(gameArea.canvas.width);
+ 		size = Math.random()*10 + 40;
+ 		Hazelnut.push(new component(size, size, "resources/hazelnut.png", nutx, nuty, "image"));
+ 	}
  	Background.update();
     Left.update();
     Right.update();
-    Hazelnut.update();
+    for(i = 0; i<Hazelnut.length; i += 1){
+    	Hazelnut[i].y += 5;
+    	Hazelnut[i].update();
+    }       
     Squirrel.update();
+    Score.text = "Pontos: " + sco + " Vidas: " + lives;
+ 	Score.update();
 }
--- master.js
+++ master.js
@@ -1,29 +1,38 @@
-//variables
-var old;
-var game = false;
-var title = false;
 var StartText;
 var StartButton;
-var TitleScreen;
 var Squirrel;
 var Hazelnut;
 var Left;
 var Right;
 var Background;
 
-//variable responsible for actual game canvas
-var gameArea = {
+//title screen (title and start button)
+function startTitle() {
+    gameArea.start();
+    Background = new component(gameArea.canvas.width, gameArea.canvas.height, "resources/bg.jpg", 0, 0, "image");
+    StartButton = new component(200, 100, "#df7126", gameArea.canvas.width / 2 - 100, gameArea.canvas.height  / 2);
+    StartText = new component(gameArea.canvas.width / 3, 0, "", 0, 0, "text");
 
-    // recreating canvas since it was deleted
+}
+
+// start game and its components
+function startGame() {
+    gameArea.start();
+    Squirrel = new component(64, 64, "resources/squirrel.png", gameArea.canvas.width / 2 - 32, gameArea.canvas.height - 64, "image");
+    Hazelnut = new component(64, 64, "resources/hazelnut.png", 30, 30, "image");
+    Left = new component(gameArea.canvas.width / 2, gameArea.canvas.height / 2, "rgba(255, 255, 255, 0.5)", 0, gameArea.canvas.height - (gameArea.canvas.height / 2));
+    Right = new component(gameArea.canvas.width / 2, gameArea.canvas.height / 2, "rgba(255, 255, 255, 0.5)", gameArea.canvas.width - gameArea.canvas.width / 2, gameArea.canvas.height - (gameArea.canvas.height / 2));
+}
+
+var gameArea = {
     canvas : document.createElement("canvas"),
 
     start : function() {
-        this.canvas.setAttribute("id", "game");
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateCanvas, 20);
+        this.interval = setInterval(updateGameArea, 20);
 
         //listening for mouse click
         window.addEventListener('mousedown', function (e) {
@@ -53,99 +62,18 @@ var gameArea = {
     
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }} 
-
-// variable responsible for menu screen canvas
-var menuScreen = {
-    // making canvas for the first time
-    canvas : document.createElement("canvas"),
-
-    start : function() {
-        this.canvas.setAttribute("id", "canvas");
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateCanvas, 20);
-
-        // listening for mouse click
-        window.addEventListener('mousedown', function (e) {
-            menuScreen.x = e.pageX;
-            menuScreen.y = e.pageY;
-        })
-
-        // listening for mouse release
-        window.addEventListener('mouseup', function (e) {
-            menuScreen.x = false;
-            menuScreen.y = false;
-        })
-
-        // listening for screen touch
-        window.addEventListener('touchstart', function (e) {
-            menuScreen.x = e.pageX;
-            menuScreen.y = e.pageY;
-        })
-
-        // listening for screen touch release
-        window.addEventListener('touchend', function (e) {
-            menuScreen.x = false;
-            menuScreen.y = false;
-        })
-        
-    },
-    
-    clear: function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }}
-
-//FUNCTIONS
-
-// title screen (title and start button)
-function startTitle() {
-    title = true;
-    menuScreen.start();
-    var msW = menuScreen.canvas.width;
-    var msH = menuScreen.canvas.height;
-    Background = new component(msW, msH, "resources/bg.jpg", 0, 0, "image");
-    TitleScreen = new component(msW, msH, "resources/title_screen.png", 0, 0, "image");
-    StartButton = new component((msW / 2.5), msH / 7, "rgba(255, 255, 255, 0.5)", (msW / 2) - (msW / 5), msH / 1.7);
-    StartText = new component(0, 0, "", 0, 0, "text");}
-
-// start game and its components
-function startGame() {
-    game = true;
-    gameArea.start();
-    var gaW = gameArea.canvas.width;
-    var gaH = gameArea.canvas.height;
-    Background = new component(gaW, gaH, "resources/bg.jpg", 0, 0, "image");
-    Squirrel = new component(64, 64, "resources/squirrel.png", gaW / 2 - 32, gaH - 64, "image");
-    Hazelnut = new component(64, 64, "resources/hazelnut.png", 30, 30, "image");
-    Left = new component(gaW / 2 + 50, gaH / 2, "rgba(255, 255, 255, 0.5)", 0, gaH - (gaH / 2), "button");
-    Right = new component(gaW / 2 + 50, gaH / 2, "rgba(255, 255, 255, 0.5)", gaW - gaW / 2, gaH - (gaH / 2), "button");}
-
-// facilitates creation of objects in current canvas
-function component(width, height, color, x, y, type) {
-    var context = menuScreen.context;
-    var msW = menuScreen.canvas.width;
-    var msH = menuScreen.canvas.height;
-    var gaW = gameArea.canvas.width;
-    var gaH = gameArea.canvas.height;
-
-    if (game) {
-    var context = gameArea.context;
     }
+}
 
+
+function component(width, height, color, x, y, type) {
     this.type = type;
     
     if (type == "image") {
         this.image = new Image();
         this.image.src = color;
     }
-
-    if (type == "button") {
-        context.fillStyle = "rgba(255, 255, 255, 0)";      
-    }
-
+    
     this.width = width;
     this.height = height;
     this.x = x;
@@ -155,77 +83,46 @@ function component(width, height, color, x, y, type) {
 
     this.update = function() {
 
+        context = gameArea.context;
+
         if (type == "image") {
         context.drawImage(this.image, this.x, this.y, this.width, this.height);
+
         } 
 
-        else if (type == "button") {
-            context.fillStyle = "rgba(255, 255, 255, 0)";      
-        }
-
-        else if (type == "text") {
-        context.font = "24px Arial";
-        context.textAlign = "center";
-        context.fillStyle = "rgba(255, 255, 255, 1)";
-        context.fillText("COMEÇAR!", (msW / 2), msH / 1.5); 
-    
-        }
-
         else {
-            context.fillStyle = "rgba(233, 141, 1, 1)";
+            context.fillStyle = "rgba(255, 255, 255, 0.5)";
             context.fillRect(this.x, this.y, this.width, this.height);
         }
 
-        
+        if (type == "text") {
+            context.font = "44px Arial";
+            context.textAlign = "center";
+            context.fillStyle = "rgba(255, 255, 255, 0.5)";
+            context.fillText("START!", gameArea.canvas.width / 2, gameArea.canvas.height  / 2 + 65); 
+    
+        }
 
     }   
 
-    // checking if any "button" was clicked
+    // checking if "buttons" were clicked
     this.clicked = function() {
     var myleft = this.x;
     var myright = this.x + (this.width);
-    var mytop = this.y;
-    var mybottom = this.y + (this.height);
     var clicked = true;
-
-    //click on menuScreen
-    if ( (title) && ((mybottom < menuScreen.y) || (mytop > menuScreen.y) || (myright < menuScreen.x) || (myleft > menuScreen.x))) {
-      clicked = false;
-    }
-
-    //click on gameArea
-    else if ( (game) && ((mybottom < gameArea.y) || (mytop > gameArea.y) || (myright < gameArea.x) || (myleft > gameArea.x))) {
-      clicked = false;
-    }
-        
-        return clicked;
-    }}   
-
-// updates canvas according to set interval (50fps in this case)
-function updateCanvas() {
-    
-    // update depending on current canvas
-    if (title) {
-        menuScreen.clear();
-    }
-    else if (game){
-        gameArea.clear();
-    }
-
-    // start game when start button is pressed
-    if (menuScreen.x && menuScreen.y) {
-        if (StartButton.clicked()) {
-            // deleting old canvas so that new canvas can be created
-            old = document.getElementById("canvas");
-            old.remove();
-            title = false;
-            clicked = false;
-            startGame();
+        if ((myright < gameArea.x) || (myleft > gameArea.x)) {
+          clicked = false;
         }
+        return clicked;
     }
 
-    // squirrel movement
-    if ((gameArea.x && gameArea.y) && (Left && Right)) {
+}   
+
+function updateGameArea() {
+    
+    gameArea.clear();
+
+    if (gameArea.x && gameArea.y) {
         if (Left.clicked()) {
           Squirrel.x += -7;
         }
@@ -234,19 +131,10 @@ function updateCanvas() {
         }
     }
 
-    // updates for gameArea
-    if (game) {
-        Background.update();
-        Squirrel.update();
-        Left.update();
-        Right.update();
-        Hazelnut.update();
-    }
-
-    // updates for titleScreen
-    if (title) {
-        Background.update();
-        TitleScreen.update();
-        StartButton.update();
-        StartText.update();
-    }}
+    Squirrel.update();
+    Left.update();
+    Right.update();
+    Hazelnut.update();
+    Background.update();
+    StartText.update();
+}
